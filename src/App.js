1b7cb751bc943a6ab701f5a928ffd669dc51cba4
unknown <natanaelcordeirobraga@gmail.com>
2024-08-23 17:21:46 -0300

    inicializing the presentation area

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      
+        <div className="presentation-area">
+          <p id="presentation-text" >Hi! My name is Natanael <br></br>
+            I am a Fullstack Developer
+          </p>
+        </div>
     </div>
   );
 }
