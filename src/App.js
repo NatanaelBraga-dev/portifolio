46452e6be81c3a282d45e27f983b9e6df9fddffb
unknown <natanaelcordeirobraga@gmail.com>
2024-08-27 17:17:55 -0300

    fix font familly config and config a profile image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './style.css';
+import neymar from './images/neymar.jpg';
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
           <p id="presentation-text" >Hi! My name is Natanael <br></br>
             I am a Fullstack Developer
           </p>
+        </div>
+        <div className="container-fotoPerfil">
+            <img src={neymar} alt="foto de perfil" id="fotoPerfil"></img>
         </div>
     </div>
   );
