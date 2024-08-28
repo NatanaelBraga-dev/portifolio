52799c169f43b41bb83a4669e0a127d04633a491
unknown <natanaelcordeirobraga@gmail.com>
2024-08-28 17:05:32 -0300

    finishing the presentation area

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './style.css';
 import neymar from './images/neymar.jpg';
+import github_icon from './icons/github_icon.png'
+import Linkedin_icon from './icons/Linkedin_icon.png'
+import instagram_icon from './icons/instagram_icon.png'
 
 function App() {
   return (
-    <div className="App">
-        <div className="presentation-area">
-          <p id="presentation-text" >Hi! My name is Natanael <br></br>
-            I am a Fullstack Developer
-          </p>
-        </div>
-        <div className="container-fotoPerfil">
+    <div id="App" className='div'>
+        <div id="presentation-area" className='div'>
+          <div id='presentation-container'>
+            <p id="presentation-text">Hi! My name is Natanael</p>
+            <p id='presentation-text-2'>I am a Fullstack Developer</p>
+          </div>  
+          <div id="container-fotoPerfil">
             <img src={neymar} alt="foto de perfil" id="fotoPerfil"></img>
+          </div>
+          <div id='container-icons'>
+            <img src={github_icon} alt='icone do github' id='icon_github'></img>
+            <img src={Linkedin_icon} alt='icone do Linkedin' id='icon_Linkedin'></img>
+            <img src={instagram_icon} alt='icone do instagram' id='icon_instagram'></img>
+          </div>
         </div>
     </div>
   );
@@ -18,6 +27,3 @@ function App() {
 
 export default App;
 
-
-
-//teste de commit
